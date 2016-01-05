6a3fc37ee05ee4eacdc9c56b2636020925b58479
Brandon <superbrandonlee@hotmail.com>
2016-01-05 20:00:19 +0000

    Added start to start

--- start.m
+++ start.m
@@ -7,14 +7,15 @@ Luck = randi([0 10],1,1);
 
 if sum(I) > 20,
     disp('You cannot allocate more than 20 stat points')
-    
+    start
 elseif sum(I) < 20,
     disp('You can allocate more stat points')
-    
+    start
 elseif sum(I) == 20
     J = input('Are you sure these are the stats you want? 0 for no, 1 for yes   ');
     if J == 0 
         disp('Start again')
+        start
     elseif J == 1
         scenario1
     end
