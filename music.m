efffd33610085eeccd53cd9fc876eba823803714
Jamie Sirett <jamiesirett@googlemail.com>
2016-01-07 20:12:49 +0000

    Changed some wording in music

--- music.m
+++ music.m
@@ -1,10 +1,10 @@
-A = input('Do you want music throughout the game? 0 for no, 1 for yes  ');
+A = input('Do you want music throughout the game? 0 for No, 1 for Yes  ');
 
 if A == 0
     start
     
  elseif A == 1
-     B = input('Please choose your track. 0 for Stormzy  1 for The Imperial March   2 for Eminem'  );
+     B = input('Please choose your track. 0 for Stormzy  1 for The Imperial March   2 for Eminem        '  );
      if B == 0
         %stormzy = audioread('stormzy.wav');
         a = audioread('stormzy.wav');
