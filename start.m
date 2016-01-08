c33710e8937b1830e8d4e49bab8bab44889727b5
Jamie Sirett <jamiesirett@googlemail.com>
2016-01-08 16:52:55 +0000

    Deleted some funky shit

--- start.m
+++ start.m
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 pause(0.5)
-=======
 clear sound
 clear all
 
->>>>>>> 478271e70f39d5f12ad191ea3f90d85d4f5972ec
 disp('.......................LOADING ROAD TO REMEDY...............................')
 pause(0.2)
 disp('                                21%')
