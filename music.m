f87c8abf6ccf180f77f39821bc5aa9c5258f1388
Ryan Lancaster <ryanlancaster@Ryans-MacBook-Air.local>
2016-01-08 14:29:33 +0000

    music

--- music.m
+++ music.m
@@ -1,24 +1,25 @@
-A = input('Do you want music throughout the game? 0 for No, 1 for Yes  ');
+
+A = input('Do you want music throughout the game?: \n (1) Yes \n (2) No \n\n')
 
 if A == 0
     start
     
  elseif A == 1
-     B = input('Please choose your track. 0 for Stormzy  1 for The Imperial March   2 for Eminem        '  );
+     B = input('Please choose your track: \n (0) Taking the Hobbits to Isengard \n (1) The Imperial March \n (2) Afro Circus \n  NOTICE: To stop music type clear sound \n\n');
      if B == 0
-        %stormzy = audioread('stormzy.wav');
-        a = audioread('stormzy.wav');
-        sound(a,50000);
+        a = audioread('hobbits.wav');
+        sound(a,45000);
      elseif B == 1
          b = audioread('starwars.wav');
-         sound(b,50000);
+         sound(b,45000);
      elseif B == 2 
-  
-
-     
- 
+         c = audioread('afrocircus.wav');
+         sound(c,45000);
+%Star Wars theme is taken from this URL: https://www.youtube.com/watch?v=Mrbc1qMnkd4
+%Hobbits theme is taken from this URL:  https://www.youtube.com/watch?v=gLf1OFM8st4    
+%Afro Circus is taken from this URL:  https://www.youtube.com/watch?v=s5mJ_1ZWmqE
          
      
-    
-     end
+         end
 end
+
