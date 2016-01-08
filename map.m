4ec7edc03a99792a640e8b25fdec56e62071e38e
Rohan Sheth <rinkusheth@hotmail.com>
2016-01-08 19:01:28 +0000

    map

---
+++ map.m
@@ -0,0 +1,281 @@
+%1
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    AP = 'https://www.google.co.uk/maps/dir/Austin+Pearce+Building,+University+of+Surrey,+Guildford,+Surrey+GU2+7XH/Austin+Pearce+Building/@51.2436646,-0.5932616,16.75z/data=!4m13!4m12!1m5!1m1!1s0x4875d0c185555555:0x273e8e5efc69aed8!2m2!1d-0.5894512!2d51.2439725!1m5!1m1!1s0x0:0x273e8e5efc69aed8!2m2!1d-0.5894512!2d51.2439725';
+    web(AP);
+    
+end
+%2
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Office = 'https://www.google.co.uk/maps/place/Centre+for+Research+in+Social+Simulation+-+CRESS/@51.243428,-0.5900911,18z/data=!4m2!3m1!1s0x0000000000000000:0x71c53b0fd95cf288';
+    web(Office);
+    
+end
+%3
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Starbucks = 'https://www.google.co.uk/maps/place/Starbucks+Coffee/@51.2429016,-0.5888626,19z/data=!4m2!3m1!1s0x0000000000000000:0x12d3c02a0a39788b';
+    web(Starbucks);
+    
+end
+%4
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Cathedral = 'https://www.google.co.uk/maps/place/Guildford+Cathedral/@51.2410134,-0.5921483,18z/data=!4m2!3m1!1s0x0000000000000000:0x51d27864fd70917e';
+    web(Cathedral);
+    
+end
+%5
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Station = 'https://www.google.co.uk/maps/place/Guildford/@51.2369348,-0.5805226,20z/data=!4m2!3m1!1s0x4875d09442f4df1f:0xf3bf0c41dd28fb0c';
+    web(Station);
+    
+end
+   
+%6
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Kingston = 'https://www.google.co.uk/maps/place/Kingston/@51.4125961,-0.3032574,17z/data=!3m1!4b1!4m2!3m1!1s0x48760bebb026ddff:0x5c35044c1665bd5f';
+    web(Kingston);
+    
+end
+%7
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Kingston = 'https://www.google.co.uk/maps/place/Kingston/@51.4125961,-0.3032574,17z/data=!3m1!4b1!4m2!3m1!1s0x48760bebb026ddff:0x5c35044c1665bd5f';
+    web(Kingston);
+    
+end
+%8
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Wimbledon = 'https://www.google.co.uk/maps/place/Wimbledon,+London/@51.4273416,-0.2589551,13z/data=!3m1!4b1!4m2!3m1!1s0x487608b7738a00c5:0x260eae2ec8a63d71';
+    web(Wimbledon);
+    
+end
+%9
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Wandsworth = 'https://www.google.co.uk/maps/place/London+Borough+of+Wandsworth,+Greater+London/@51.4518171,-0.2278435,13z/data=!3m1!4b1!4m2!3m1!1s0x48760589ff8fea83:0x37252c9ca56f68d2';
+    web(Wandsworth);
+    
+end
+%10
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    PutneyBridge = 'https://www.google.co.uk/maps/place/Putney+Bridge,+London+SW6+3JD/@51.4667798,-0.2153056,17z/data=!3m1!4b1!4m2!3m1!1s0x48760f0ca20ed70d:0xc47a74350b964574';
+    web(PutneyBridge);
+    
+end
+%11
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    FulhamPalace = 'https://www.google.co.uk/maps/place/Fulham+Palace/@51.470557,-0.218036,17z/data=!3m1!4b1!4m2!3m1!1s0x48760fa06dd36177:0xeda410c8dfe100cc';
+    web(FulhamPalace);
+    
+end
+%12
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    CravenCottage = 'https://www.google.co.uk/maps/place/Craven+Cottage/@51.4748946,-0.2239739,17z/data=!3m1!4b1!4m2!3m1!1s0x48760fa7a4488157:0xa0377f648d942d05';
+    web(CravenCottage);
+    
+end
+%13
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    TheFlowerWalk = 'https://www.google.co.uk/maps/place/The+Flower+Walk,+London+W2+3XA/@51.5025883,-0.1830268,17z/data=!3m1!4b1!4m2!3m1!1s0x4876055a3bb3c4a9:0x7cd001f56a9a414e';
+    web(TheFlowerWalk);
+    
+end
+%14
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    BuckinghamPalace= 'https://www.google.co.uk/maps/place/Buckingham+Palace/@51.501364,-0.144084,17z/data=!3m1!4b1!4m2!3m1!1s0x48760520cd5b5eb5:0xa26abf514d902a7';
+    web(BuckinghamPalace);
+    
+end
+%15
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    BuckinghamPalace= 'https://www.google.co.uk/maps/place/Buckingham+Palace/@51.501364,-0.144084,17z/data=!3m1!4b1!4m2!3m1!1s0x48760520cd5b5eb5:0xa26abf514d902a7';
+    web(BuckinghamPalace);
+    
+end
+%16
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    A= '';
+    web(A);
+    
+end
+
+%strand2 1
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    WimbledonTennis= 'https://www.google.co.uk/maps/place/Wimbledon+Park+Tennis+Court/@51.435656,-0.2048301,17z/data=!3m1!4b1!4m2!3m1!1s0x48760f4b875b6f2f:0xd78b2de019f2cfc3';
+    web(WimbledonTennis);
+    
+end
+%strand2 2
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    EnglandLawnTennis = 'https://www.google.co.uk/maps/place/The+All+England+Lawn+Tennis+Club+Centre+Court/@51.4337283,-0.2162504,17z/data=!3m1!4b1!4m2!3m1!1s0x48760f3453631b3b:0xd1db13348fafadd0';
+    web(EnglandLawnTennis);
+    
+end
+%stand2 3
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Shop = 'https://www.google.co.uk/maps/place/Convenience+Store/@51.4442693,-0.2071603,21z/data=!4m2!3m1!1s0x0000000000000000:0xf1dac91475ce727e'
+    web(Shop)
+end
+%strand2 4
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Southfields= 'https://www.google.co.uk/maps/place/Southfields,+London+SW18/@51.447491,-0.201883,17z/data=!3m1!4b1!4m2!3m1!1s0x48760f434c037acd:0x7034358959c615b';
+    web(Southfields);
+    
+end
+
+%strand3 1
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+   LaBaita= 'https://www.google.co.uk/maps/place/La+Baita/@51.4583619,-0.1508096,17z/data=!3m1!4b1!4m2!3m1!1s0x487605b38cd3b3a5:0x1c1a4908ce2e4815';
+    web(Labaita);
+    
+end
+%strand3 2
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Battersea= 'https://www.google.co.uk/maps/place/Battersea+Power+Station/@51.4818235,-0.1465923,17z/data=!3m1!4b1!4m2!3m1!1s0x487604fd7f539fe1:0x96a0bfdb4e42515d';
+    web(Battersea);
+    
+end
+%strand3 3
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Sainsburys= 'https://www.google.co.uk/maps/place/Sainsbury`s+Local/@51.4882263,-0.1517851,18z/data=!4m5!1m2!2m1!1ssainsburies!3m1!1s0x0000000000000000:0x52f29f029a52c4ff';
+    web(Sainsburys);
+    
+end
+%strand3 4
+Map = input('Do you want to view your map? 0 for No or 1 for Yes    ');
+
+if Map == 0 ;
+   disp('')
+elseif Map == 1 ;
+    disp('Loading Map.....')
+    Harrods= 'https://www.google.co.uk/maps/place/Harrods/@51.4994055,-0.1654231,17z/data=!3m1!4b1!4m2!3m1!1s0x48760538ed2a30ff:0x15ee630d4b4e0d42';
+    web(Harrods);
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
