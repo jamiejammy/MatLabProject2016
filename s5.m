c70a06ce5e842c5eb56a220af76d50c5c1ab2080
Jamie Sirett <jamiesirett@googlemail.com>
2016-01-08 17:15:13 +0000

    Made s5

---
+++ s5.m
@@ -0,0 +1,24 @@
+disp('                                                         ')
+disp('You see a rusty old minivan driving through as you wave at them to come help you.')
+disp('They stop for you as you jump onto the minivan and get away. They stop ahead to let you in the car.')
+disp('The driver is a lady and the other passenger is her daughter and are planning to drive to Canary Wharf.')
+disp('On your way to London you realise the daughter is turning into a zombie without the driver knowing. Do you?')
+disp(' ')
+disp('1. Ignore it, turn on the radio and start a rave in the car. Luckily you find a bottle of vodka in the glovebox.')
+disp('2. Kill the daughter, strangling her with her own seatbelt. ')
+disp('3. Act sick so the mother stops and lets you out of the car. You can make your own way.   ')
+disp(' ')
+L = input('Choose by typing 1, 2 or 3     ')
+disp(' ')
+if L == 1
+   disp('You start getting into the zone and get completely wasted.')
+   disp('You ignore the daughter completely as she turns into a full zombie and bites your leg off as the blood squirts everywhere.')
+   disp('You slowly start turning yourself and starting feeding upon the mother.')
+   death
+elseif L == 2
+   disp(': You kill the daughter. ')
+   s6a
+elseif L == 3
+     disp('The mother lets you out of the car, without a companion your journey is more simple. ')
+     s6
+end
