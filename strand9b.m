d33d9eab4628f57e1efc1469d099baa92a3baab7
Ryan Lancaster <ryanlancaster@Ryans-MacBook-Air.local>
2016-01-09 00:16:50 +0000

    9b update

--- strand9b.m
+++ strand9b.m
@@ -14,10 +14,10 @@ end
 pause(0.5)
 disp('                                                         ')
 pause(0.5)
-disp('You continue across the river, getting hungry.   ')
-pause(0.5)
+disp('You continue across the river, getting hungry...   ')
+pause(1.5)
 disp('There is a Sainsburys next to you, but it looks like there is some zombies in there.   ')
-pause(0.5)
+pause(1.5)
 disp('Do you?  ')
 disp('  ')
 disp('  ')
@@ -32,17 +32,24 @@ pause(0.5)
 disp(' ')
 if L == 1;
    disp('You go in all guns blazing, killing all the zombies...  ')
+   pause(1.5)
    disp('bar three that are behind the meat counter.  ')
-   pause(0.5)
+   pause(1.5)
    disp('You then proceed to use the trolleys as a battering ram too... ')
+   pause(1.5)
    disp('knock over the counter and kill the zombies.')
+   pause(1.5)
    disp('You help yourself to some Dorito’s and a Coke. ')
+   pause(1.5)
    disp('You feel better afterwards and gain 1 agility point.')
+   pause(1.5)
    Agility = Agility + 1;
    strand10b
 elseif L == 2;
     disp('You continue on your way to the Palace, but you are feeling weak.  ')
+    pause(1.5)
     disp('You lose 1 agility point. ')
+    pause(1)
     Agility = Agility - 1;
     strand10b
 end
