748c91585c76e15c9433cd3e571b565fff58aa46
Jamie Sirett <jamiesirett@googlemail.com>
2016-01-09 01:06:08 +0000

    Modified winning script and adding credit gif

--- winning.m
+++ winning.m
@@ -4,24 +4,31 @@ clear sound
         
 [A,map]=imread('wingif.gif','frames','all'); 
 win=immovie(A,map); 
-w = implay(win, 5);
+w = implay(win, 4);
 a = audioread('hobbits.wav');
-        sound(a,45000);1
+        sound(a,45000);
 set(findall(0,'tag','spcui_scope_framework'),'position',[500 100 700 951]);
 play(w.DataSource.Controls);
-pa = 4;
+pa = 6;
 pause(pa)
 close(w);
+clear sound
 
-
-STA = input('Would you like to restart the game?      1 for Yes, 0 for No')
+STA = input('Would you like to restart the game?      1 for Yes, 0 for No             ');
 if STA == 1
     start
 elseif STA == 0
-    
-pause(0.5)
-clear sound
-    %% ADD CREDITS
+    [A,map]=imread('credgif.gif','frames','all'); 
+    cre=immovie(A,map); 
+    c = implay(cre, 8);
+    set(findall(0,'tag','spcui_scope_framework'),'position',[500 100 700 951]);
+    play(c.DataSource.Controls);
+    pa = 7;
+    pause(pa)
+    close(c);
+    disp(' ')
+    disp('Thank you for playing!')
+    disp(' ')
 else 
     disp('Please enter a 1 or a 0')
     death
