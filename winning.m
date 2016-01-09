6459fda1989f3de8682569a580b71067386d0b01
Jamie Sirett <jamiesirett@googlemail.com>
2016-01-09 00:31:42 +0000

    Added winning gif and winning script

---
+++ winning.m
@@ -0,0 +1,22 @@
+disp('  ')
+disp('  ')
+
+
+[A,map]=imread('wingif.gif','frames','all'); 
+win=immovie(A,map); 
+w = implay(win, 5);
+set(findall(0,'tag','spcui_scope_framework'),'position',[500 100 700 951]);
+play(w.DataSource.Controls);
+pa = 4;
+pause(pa)
+close(w);
+
+STA = input('Would you like to restart the game?      1 for Yes, 0 for No')
+if STA == 1
+    start
+elseif STA == 0
+    %% ADD CREDITS
+else 
+    disp('Please enter a 1 or a 0')
+    death
+end
