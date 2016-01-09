5380a429500dff3a31f56324b090df967f09ae51
Rohan Sheth <rinkusheth@hotmail.com>
2016-01-09 15:20:43 +0000

    proper email coded

--- s13.m
+++ s13.m
@@ -27,7 +27,7 @@ pause(0.5)
 disp(' ')
 if L == 1;
     setprefgmail
-    K = input('Enter your email address here with an apostrophe at the beginning and the end:    ');
+   K = input('Enter your email address here with ','s'   );
     disp(' ')
     emailaddress = {K};
     sendmail(emailaddress, 'PIN for the missing ingredient!', '1337');
