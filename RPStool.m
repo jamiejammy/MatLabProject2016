462f8669037cb366366f2b7bfe12e32a450c744f
Brandon <superbrandonlee@hotmail.com>
2016-01-10 23:03:10 +0000

    Added comments

--- RPStool.m
+++ RPStool.m
@@ -22,8 +22,8 @@ function varargout = RPStool(varargin)
 
 % Edit the above text to modify the response to help RPStool
 
-% Last Modified by GUIDE v2.5 08-Jan-2016 01:47:47
-% #Made by Brandon Lee#
+% Last Modified by GUIDE v2.5 08-Jan-2016 03:47:47
+%                                         ##Made by Brandon Lee##
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -46,12 +46,6 @@ end
 
 % --- Executes just before RPStool is made visible.
 function RPStool_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to RPStool (see VARARGIN)
-
 % Choose default command line output for RPStool
 handles.output = hObject;
 
@@ -64,67 +58,106 @@ uiwait(handles.figure1);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = RPStool_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
 % Get default command line output from handles structure
 varargout{1} = str2double(get(handles.hscore,'String'));
+% hscore [Human score] is an output.
 varargout{2} = str2double(get(handles.tscore,'String'));
+% tscore [Number of ties] is an output.
 varargout{3} = str2double(get(handles.cscore,'String'));
+% cscore [Julie's score] is an output.
 varargout{4} = str2double(get(handles.round,'String'));
+% round [Number of rounds played] is an output.
 assignin ('base','player_score',varargout{1});
+% Assign hscore with the name 'player_score' and store it in the workspace
 assignin ('base','tied_score',varargout{2});
-assignin ('base','comp_score',varargout{3});
+% Assign tscore with the name 'tie_score' and store it in the workspace
+assignin ('base','julie_score',varargout{3});
+% Assign cscore with the name 'julie_score' and store it in the workspace
 assignin ('base','num_rounds',varargout{4});
-% The figure can be deleted now
+% Assign round with the name 'num_rounds' and store it in the workspace
 delete(handles.figure1);
+% The figure can be deleted now
 
 
 % --- Executes on button press in Rock.
 function Rock_Callback(hObject, eventdata, handles)
 % hObject    handle to Rock (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 Computer = randi([1 3]);
+% Computer is a randomly generated number between 1 and 3, this will be
+% used to determine what Julie plays.
 set(handles.hplays,'String','You play rock');
+% hplays is a static text box and displays what move the player plays. When
+% the player presses the 'Rock' button, hplays displays 'You play rock'
+
+% I've assigned rock as 1, paper as 2, and scissors as 3.
 if Computer == 1
+    % Following things happen if Computer == 1, i.e. Rock
     set(handles.cplays,'String','Julie plays rock');
+    % cplays is a static text box next to hplays, it displays what move
+    % Julie plays. It changes to 'Julie plays rock' in this case.
     set(handles.winner,'String','It’s a tie!');
+    % winner is a static text box which displays who has won the round. In
+    % this case it displays 'It’s a tie!'
     a = str2double(get(handles.tscore,'String'));
+    % This acquires tscore, which is a static text box displaying the
+    % number of ties. It turns it from a string into a double (i.e number)
+    % and assigns it the variable 'a'.
     a = a+1;
+    % Number of ties increases by 1.
     set(handles.tscore,'String',a);
+    % Set tscore to the new number of ties.
     d = str2double(get(handles.round,'String'));
+    % This acquires round, which is a static text box displaying the
+    % number of rounds played. It turns it from a string into a double (i.e number)
+    % and assigns it the variable 'd'.
     d = d+1;
+    % Number of rounds played increases by 1.
     set(handles.round,'String',d);
+    % Set round to the new number of rounds played.
 elseif Computer == 2
+    % Following things happen if Computer == 2, i.e. Paper
     set(handles.cplays,'String','Julie plays paper');
+    % cplays displays 'Julie plays paper'
     set(handles.winner,'String','Julie wins!');
+    % winner displays 'Julie wins!'
     b = str2double(get(handles.cscore,'String'));
+    % Acquire cscore and call it 'b'
     b = b+1;
+    % Computer won, so Julie's score increases by 1.
     set(handles.cscore,'String',b);
+    % Set the new cscore.
     d = str2double(get(handles.round,'String'));
+    % Acquire round [number of rounds] and call it 'd' 
     d = d+1;
+    % Number of rounds played increased by 1
     set(handles.round,'String',d);
+    % Set the new number of rounds played.
 else
+    % Following things happen if Computer == 2, i.e. Paper
     set(handles.cplays,'String','Julie plays scissors');
+    % cplays displays 'Julie plays scissors'
     set(handles.winner,'String','You win!')
+    % winner displays 'Julie wins!'
     c = str2double(get(handles.hscore,'String'));
+    % Acquire hscore and call it 'c'
     c = c+1;
+    % Computer won, so Julie's score increases by 1.
     set(handles.hscore,'String',c);
+    % Set new hscore
     d = str2double(get(handles.round,'String'));
+    % Acquire round [number of rounds] and call it 'd' 
     d = d+1;
+    % Number of rounds played increased by 1
     set(handles.round,'String',d);
+    % Set the new number of rounds played.
 end
 
-
+% The code is pretty much the same for the Paper and Scissors button, so
+% the comments from above can be applied below.
 
 % --- Executes on button press in Paper.
 function Paper_Callback(hObject, eventdata, handles)
 % hObject    handle to Paper (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 Computer = randi([1 3]);
 set(handles.hplays,'String','You play paper');
 if Computer == 1
@@ -159,8 +192,6 @@ end
 % --- Executes on button press in Scissors.
 function Scissors_Callback(hObject, eventdata, handles)
 % hObject    handle to Scissors (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 Computer = randi([1 3]);
 set(handles.hplays,'String','You play scissors');
 if Computer == 1
@@ -193,33 +224,42 @@ else
 end
 
 
-% --- Executes on button press in reset.
+% --- Executes when the Reset button is pressed.
 function reset_Callback(hObject, eventdata, handles)
 % hObject    handle to reset (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 a = str2double(get(handles.tscore,'String'));
+% Acquire tscore, call it 'a'.
 a = 0;
+% Make 'a' equal to zero.
 set(handles.tscore,'String',a);
+% Set new 'a'. So tscore displays 0
 b = str2double(get(handles.cscore,'String'));
+% Acquire cscore, call it 'b'.
 b = 0;
+% Make 'b' equal to zero.
 set(handles.cscore,'String',b);
+% Set new 'b'. So cscore displays 0
 c = str2double(get(handles.hscore,'String'));
+% Acquire hscore, call it 'c'.
 c = 0;
+% Make 'c' equal to zero.
 set(handles.hscore,'String',c);
-d= 0;
+% Set new 'c'. So hscore displays 0
+d = 0;
+% Make 'a' equal to zero.
 set(handles.round,'String',d);
+% Set new 'd' So round displays 0
 set(handles.hplays,'String','You play ');
+% Set hplays to display "You play"
 set(handles.cplays,'String','Julie plays ');
+% Set cplays to display "Julie plays"
 set(handles.winner,'String',' ')
+% Set winner to display nothing.
 
 
 % --- Executes when user attempts to close figure1.
 function figure1_CloseRequestFcn(hObject, eventdata, handles)
 % hObject    handle to figure1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
 if isequal(get(hObject, 'waitstatus'), 'waiting')
     % The GUI is still in UIWAIT, us UIRESUME
     uiresume(hObject);
--- RockPaperScissors.m
+++ RockPaperScissors.m
@@ -5,13 +5,17 @@ function [NumWin, NumTie] = RockPaperScissors(n)
     if nargin < 1;
         n = [];
     end
+    % if the number of input arguments is less than 1, generate an empty matrix named n
+    % which is the number of rounds.
     if isempty(n)
         n = 1;
     end
+    % if the variable n is empty, then n = 1, i.e. play one rounds.
     
     NumWin = 0; 
     NumLose = 0;
     NumTie = 0;
+    %Initial values of NumWin, NumLose and NumTie are zero.
     
     A = 'Tie';
     B = 'Loss';
@@ -20,18 +24,29 @@ function [NumWin, NumTie] = RockPaperScissors(n)
     
     for i = 1:n
         Player = input('Choose 1 for rock, 2 for paper, or 3 for scissors:     ');
-        AI = randi([1 3],1,1);
+        AI = randi([1 3]);
+        % AI generates a random number between 1 and 3, this will be the
+        % computer's move.
         if Player == 1
+            % If the player chooses rock, these are the possible results.
             if AI == 1
+                % 1 represents rock, rock and rock is a tie.
                 Result = A;
+                % A is the string 'Tie'.
                 NumTie = NumTie + 1;
+                % Number of ties increases by 1.
             elseif AI == 2
+                % 2 represents paper, paper wins against rock.
                 Result = B;
+                % B is the string 'Loss'.
                 NumLose = NumLose + 1;
+                % Number of losses increases by 1.
             else Result = C;
+                % Otherwise, increase number of wins by 1.
                 NumWin = NumWin + 1;
             end
         elseif Player == 2
+            % This elseif and the one below is similar to the one above.
             if AI == 1
                 Result = C;
                 NumWin = NumWin + 1;
@@ -52,6 +67,8 @@ function [NumWin, NumTie] = RockPaperScissors(n)
                 NumTie = NumTie + 1;
             end
         else
+            %If the player chooses an option outside of 1, 2 or 3, they
+            %lose.
             Result = D;
             NumLose = NumLose + 1;
         end
@@ -62,6 +79,8 @@ function [NumWin, NumTie] = RockPaperScissors(n)
             AI = 'Paper.';
         else AI = 'Scissors.';
         end
+        % This is used for displaying what the player has chosen to play,
+        % utilised below.
 
         if Player == 1
             Player = 'Rock.';
@@ -72,34 +91,49 @@ function [NumWin, NumTie] = RockPaperScissors(n)
         else
             Player = 'nothing at all!';         
         end
+        % This is used for displaying what the AI has chosen to play, it is
+        % utilised below.
 
         disp(' ')
+        % Empty disp for presentation, not so cluttered.
         pause(0.3)
+        % Pause for the game to flow smoothly as opposed to coming out in
+        % one big block.
         disp(['You chose ',num2str(Player)])
+        % Displays what the player has chosen.
         pause(0.3)
         disp(' ')
         pause(0.3)
         disp(['Computer chose ',num2str(AI)])
+        % Displays what the AI has chosen.
         pause(0.3)
         disp(' ')
 
         if strcmp(Result,A) == 1
+            % String comparison of the variable Result and A, if these two
+            % are the same, then display 'It is a tie!'.
             pause(0.3)
             disp('It is a tie!')
             pause(0.3)
             disp(' ')
         elseif strcmp(Result,B) == 1
+            % String comparison of the variable Result and B, if these two
+            % are the same, then display 'You lose!'.
             pause(0.3)
             disp('You lose!')
             pause(0.3)
             disp(' ')
         elseif strcmp(Result,C) == 1
+            % String comparison of the variable Result and C, if these two
+            % are the same, then display 'You win!'.
             pause(0.3)
             disp('You win!')
             pause(0.3)
             disp(' ')
         else
             pause(0.3)
+            % If the player didn't choose 1, 2 or 3 as their input, then
+            % display 'You lose! Choose a valid input next time.'
             disp('You lose! Choose a valid input next time.')
             pause(0.3)
         end
